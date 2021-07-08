EdenEast/Gemcutter

Widgets are now updated every frame

Engine/Jewel3D/Application/Application.cpp
@@ -1,8 +1,10 @@
 // Copyright (c) 2017 Emilian Cioca
 #include "Jewel3D/Precompiled.h"
 #include "Application.h"
-#include "Logging.h"
-#include "Timer.h"
+#include "Jewel3D/Application/Logging.h"
+#include "Jewel3D/Application/Timer.h"
+#include "Jewel3D/GUI/Button.h"
+#include "Jewel3D/GUI/Widget.h"
 #include "Jewel3D/Input/Input.h"
 #include "Jewel3D/Rendering/Light.h"
 #include "Jewel3D/Rendering/ParticleEmitter.h"
@@ -385,7 +387,7 @@ namespace Jwl
 			__int64 currentTime = Timer::GetCurrentTick();
 			if (currentTime - lastFpsCapture >= Timer::GetTicksPerSecond())
 			{
-				// We don't want to update the timer variable with "+= 1.0" here. After a lag spike this 
+				// We don't want to update the timer variable with "+= 1.0" here. After a lag spike this
 				// would cause FPS to suddenly be recorded more often than once a second.
 				lastFpsCapture = currentTime;
 
@@ -439,6 +441,13 @@ namespace Jwl
 		EventQueue.Dispatch();
 
 		// Update engine components.
+		Widget::UpdateAll();
+
+		for (auto& button : All<Button>())
+		{
+			button.Update();
+		}
+
 		for (Entity& entity : With<ParticleUpdaterTag>())
 		{
 			entity.Get<ParticleEmitter>().Update();

EmilianC/Jewel3D

gemcutter/Application/Application.cpp
@@ -2,6 +2,8 @@
 #include "Application.h"
 #include "gemcutter/Application/Logging.h"
 #include "gemcutter/Application/Timer.h"
+#include "gemcutter/GUI/Button.h"
+#include "gemcutter/GUI/Widget.h"
 #include "gemcutter/Input/Input.h"
 #include "gemcutter/Rendering/Light.h"
 #include "gemcutter/Rendering/ParticleEmitter.h"
@@ -439,6 +441,13 @@ namespace gem
 		EventQueue.Dispatch();
 
 		// Update engine components.
+		Widget::UpdateAll();
+
+		for (auto& button : All<Button>())
+		{
+			button.Update();
+		}
+
 		for (Entity& entity : With<ParticleUpdaterTag>())
 		{
 			entity.Get<ParticleEmitter>().Update();