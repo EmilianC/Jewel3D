EdenEast/Gemcutter

First pass on basic Widget types
- Added rectangle class to represent widget bounds
- Widget: Core layout element
- Screen: Root widget sized to the viewport
- Image: A widget containing a sprite
- Button: A widget responding to mouse events

Engine/Jewel3D/GUI/Image.cpp
@@ -1,0 +1,47 @@
+// Copyright (c) 2020 Emilian Cioca
+#include "Jewel3D/Precompiled.h"
+#include "Image.h"
+#include "Jewel3D/Rendering/Sprite.h"
+#include "Jewel3D/Resource/Material.h"
+
+namespace Jwl
+{
+	Image::Image(Entity& _owner)
+		: Widget(_owner)
+	{
+		auto material = Material::MakeNew();
+		material->depthMode = DepthFunc::None;
+		material->shader = Load<Shader>("Shaders/Default/Sprite");
+
+		auto& sprite = owner.Require<Sprite>();
+		sprite.SetAlignment(Alignment::Center);
+		sprite.SetMaterial(std::move(material));
+	}
+
+	Image::Image(Entity& owner, Texture::Ptr tex)
+		: Image(owner)
+	{
+		SetTexture(std::move(tex));
+	}
+
+	void Image::SetTexture(Texture::Ptr tex)
+	{
+		texture = std::move(tex);
+
+		auto material = owner.Get<Renderable>().GetMaterial();
+		material->textures.Clear();
+		material->textures.Add(texture);
+	}
+
+	Texture::Ptr Image::GetTexture()
+	{
+		return texture;
+	}
+
+	void Image::UpdateContent()
+	{
+		const Rectangle& bounds = GetAbsoluteBounds();
+
+		owner.scale = vec3(bounds.width, bounds.height, 1.0f);
+	}
+}

Engine/Jewel3D/GUI/Widget.cpp
@@ -1,0 +1,134 @@
+// Copyright (c) 2020 Emilian Cioca
+#include "Jewel3D/Precompiled.h"
+#include "Widget.h"
+#include "Jewel3D/Entity/Hierarchy.h"
+
+#ifdef _DEBUG
+#include "Jewel3D/Rendering/Primitives.h"
+#include "Jewel3D/Rendering/Rendering.h"
+#include "Jewel3D/Utilities/Random.h"
+#endif
+
+namespace Jwl
+{
+	Widget::Widget(Entity& _owner)
+		: Component(_owner)
+	{
+		owner.Require<Hierarchy>().propagateTransform = false;
+
+#ifdef _DEBUG
+		// Normalize to keep the intensity is reliable.
+		debugColor = Normalize(RandomColor());
+#endif
+	}
+
+	void Widget::SetTransform(vec2 position, vec2 size)
+	{
+		const float halfWidth  = size.x * 0.5f;
+		const float halfHeight = size.y * 0.5f;
+
+		top.offset    = halfHeight;
+		bottom.offset = -halfHeight;
+
+		left.offset  = -halfWidth;
+		right.offset = halfWidth;
+
+		//...
+
+		UpdateContent();
+	}
+
+	void Widget::FitToParent()
+	{
+		top.anchor = 0.0f;
+		top.offset = 0.0f;
+
+		bottom.anchor = 0.0f;
+		bottom.offset = 0.0f;
+
+		left.anchor = 0.0f;
+		left.offset = 0.0f;
+
+		right.anchor = 0.0f;
+		right.offset = 0.0f;
+	}
+
+	Entity::Ptr Widget::CreateChildWidget()
+	{
+		auto entity = owner.Get<Hierarchy>().CreateChild();
+		entity->Add<Widget>();
+
+		return entity;
+	}
+
+	void Widget::UpdateAll()
+	{
+		for (Entity& entity : With<Widget, HierarchyRoot>())
+		{
+			auto& widget = entity.Get<Widget>();
+			widget.absoluteBounds.width = widget.right.offset;
+			widget.absoluteBounds.height = widget.top.offset;
+
+			for (Entity::Ptr& child : entity.Get<Hierarchy>().GetChildren())
+			{
+				if (auto* childWidget = child->Try<Widget>())
+				{
+					childWidget->UpdateBounds(widget);
+				}
+			}
+		}
+	}
+
+#ifdef _DEBUG
+	void Widget::DrawOutline() const
+	{
+		// Will have to make sure a camera is bound.
+
+		vec3 bottomLeft{ absoluteBounds.x, absoluteBounds.y, 0.0f };
+		vec3 topLeft{ absoluteBounds.x, absoluteBounds.y + absoluteBounds.height, 0.0f };
+		vec3 topRight{ absoluteBounds.x + absoluteBounds.width, absoluteBounds.y + absoluteBounds.height, 0.0f };
+		vec3 bottomRight{ absoluteBounds.x + absoluteBounds.width, absoluteBounds.y, 0.0f };
+
+		Primitives.DrawRectangle(bottomLeft, topLeft, topRight, bottomRight, vec4(debugColor, 1.0f));
+
+		// Draw anchors:
+		SetBlendFunc(BlendFunc::Linear);
+		//Primitives.DrawLine(, , vec4(debugColor, 0.5f));
+		//Primitives.DrawLine(, , vec4(debugColor, 0.5f));
+		//Primitives.DrawLine(, , vec4(debugColor, 0.5f));
+		//Primitives.DrawLine( , vec4(debugColor, 0.5f));
+	}
+#endif
+
+	void Widget::UpdateBounds(const Widget& parent)
+	{
+		const float offsetFromTop    = parent.absoluteBounds.height * top.anchor + top.offset;
+		const float offsetFromBottom = parent.absoluteBounds.height * bottom.anchor + bottom.offset;
+		const float offsetFromLeft   = parent.absoluteBounds.width * left.anchor + left.offset;
+		const float offsetFromRight  = parent.absoluteBounds.width * right.anchor + right.offset;
+
+		const float topEdge    = (parent.absoluteBounds.y + parent.absoluteBounds.height) - offsetFromTop;
+		const float bottomEdge = parent.absoluteBounds.y + offsetFromBottom;
+		const float leftEdge   = parent.absoluteBounds.x + offsetFromLeft;
+		const float rightEdge  = (parent.absoluteBounds.x + parent.absoluteBounds.width) - offsetFromRight;
+
+		absoluteBounds.x = leftEdge;
+		absoluteBounds.y = bottomEdge;
+		absoluteBounds.width = rightEdge - leftEdge;
+		absoluteBounds.height = topEdge - bottomEdge;
+
+		// The actual position of the entity is the pivot (center of the widget).
+		owner.position.x = absoluteBounds.x + absoluteBounds.width * 0.5f;
+		owner.position.y = absoluteBounds.y + absoluteBounds.height * 0.5f;
+
+		UpdateContent();
+
+		for (Entity::Ptr& child : owner.Get<Hierarchy>().GetChildren())
+		{
+			if (auto* childWidget = child->Try<Widget>())
+			{
+				childWidget->UpdateBounds(*this);
+			}
+		}
+	}
+}

Engine/Jewel3D/GUI/Widget.h
@@ -1,0 +1,57 @@
+// Copyright (c) 2020 Emilian Cioca
+#pragma once
+#include "Jewel3D/Entity/Entity.h"
+#include "Jewel3D/GUI/Rectangle.h"
+
+namespace Jwl
+{
+	struct Edge
+	{
+		float anchor = 0.0f;
+		float offset = 0.0f;
+	};
+
+	// A generic UI element sized with respect to its parent widget.
+	class Widget : public Component<Widget>
+	{
+	public:
+		Widget(Entity& owner);
+
+		// Helper function which sets anchors to maintain the target size and position.
+		// Position is in the space of the parent widget.
+		void SetTransform(vec2 position, vec2 size);
+
+		// Sets the anchors to the full space available in the parent widget.
+		// This is the default behaviour.
+		void FitToParent();
+
+		Entity::Ptr CreateChildWidget();
+
+		const Rectangle& GetAbsoluteBounds() const { return absoluteBounds; }
+
+		static void UpdateAll();
+
+		// Called when the bounds of the widget are updated.
+		// This is where specific widgets can adjust their content.
+		virtual void UpdateContent() {}
+
+#ifdef _DEBUG
+		void DrawOutline() const;
+		vec3 debugColor;
+#endif
+
+		// Clips the rendering of child widgets within the bounds of this widget.
+		bool clipChildren = true;
+
+		Edge top;
+		Edge bottom;
+		Edge left;
+		Edge right;
+
+	private:
+		void UpdateBounds(const Widget& parent);
+
+		// World-space demensions of the widget
+		Rectangle absoluteBounds;
+	};
+}

EmilianC/Jewel3D

gemcutter/GUI/Image.cpp
@@ -1,0 +1,46 @@
+// Copyright (c) 2021 Emilian Cioca
+#include "Image.h"
+#include "gemcutter/Rendering/Sprite.h"
+#include "gemcutter/Resource/Material.h"
+
+namespace gem
+{
+	Image::Image(Entity& _owner)
+		: Widget(_owner)
+	{
+		auto material = Material::MakeNew();
+		material->depthMode = DepthFunc::None;
+		material->shader = Load<Shader>("Shaders/Default/Sprite");
+
+		auto& sprite = owner.Require<Sprite>();
+		sprite.SetAlignment(Alignment::Center);
+		sprite.SetMaterial(std::move(material));
+	}
+
+	Image::Image(Entity& owner, Texture::Ptr tex)
+		: Image(owner)
+	{
+		SetTexture(std::move(tex));
+	}
+
+	void Image::SetTexture(Texture::Ptr tex)
+	{
+		texture = std::move(tex);
+
+		auto material = owner.Get<Renderable>().GetMaterial();
+		material->textures.Clear();
+		material->textures.Add(texture);
+	}
+
+	Texture::Ptr Image::GetTexture()
+	{
+		return texture;
+	}
+
+	void Image::UpdateContent()
+	{
+		const Rectangle& bounds = GetAbsoluteBounds();
+
+		owner.scale = vec3(bounds.width, bounds.height, 1.0f);
+	}
+}

gemcutter/GUI/Widget.cpp
@@ -1,0 +1,133 @@
+// Copyright (c) 2021 Emilian Cioca
+#include "Widget.h"
+#include "gemcutter/Entity/Hierarchy.h"
+
+#ifdef _DEBUG
+#include "gemcutter/Rendering/Primitives.h"
+#include "gemcutter/Rendering/Rendering.h"
+#include "gemcutter/Utilities/Random.h"
+#endif
+
+namespace gem
+{
+	Widget::Widget(Entity& _owner)
+		: Component(_owner)
+	{
+		owner.Require<Hierarchy>().propagateTransform = false;
+
+#ifdef _DEBUG
+		// Normalize to keep the intensity is reliable.
+		debugColor = Normalize(RandomColor());
+#endif
+	}
+
+	void Widget::SetTransform(vec2 position, vec2 size)
+	{
+		const float halfWidth  = size.x * 0.5f;
+		const float halfHeight = size.y * 0.5f;
+
+		top.offset    = halfHeight;
+		bottom.offset = -halfHeight;
+
+		left.offset  = -halfWidth;
+		right.offset = halfWidth;
+
+		//...
+
+		UpdateContent();
+	}
+
+	void Widget::FitToParent()
+	{
+		top.anchor = 0.0f;
+		top.offset = 0.0f;
+
+		bottom.anchor = 0.0f;
+		bottom.offset = 0.0f;
+
+		left.anchor = 0.0f;
+		left.offset = 0.0f;
+
+		right.anchor = 0.0f;
+		right.offset = 0.0f;
+	}
+
+	Entity::Ptr Widget::CreateChildWidget()
+	{
+		auto entity = owner.Get<Hierarchy>().CreateChild();
+		entity->Add<Widget>();
+
+		return entity;
+	}
+
+	void Widget::UpdateAll()
+	{
+		for (Entity& entity : With<Widget, HierarchyRoot>())
+		{
+			auto& widget = entity.Get<Widget>();
+			widget.absoluteBounds.width = widget.right.offset;
+			widget.absoluteBounds.height = widget.top.offset;
+
+			for (Entity::Ptr& child : entity.Get<Hierarchy>().GetChildren())
+			{
+				if (auto* childWidget = child->Try<Widget>())
+				{
+					childWidget->UpdateBounds(widget);
+				}
+			}
+		}
+	}
+
+#ifdef _DEBUG
+	void Widget::DrawOutline() const
+	{
+		// Will have to make sure a camera is bound.
+
+		vec3 bottomLeft{ absoluteBounds.x, absoluteBounds.y, 0.0f };
+		vec3 topLeft{ absoluteBounds.x, absoluteBounds.y + absoluteBounds.height, 0.0f };
+		vec3 topRight{ absoluteBounds.x + absoluteBounds.width, absoluteBounds.y + absoluteBounds.height, 0.0f };
+		vec3 bottomRight{ absoluteBounds.x + absoluteBounds.width, absoluteBounds.y, 0.0f };
+
+		Primitives.DrawRectangle(bottomLeft, topLeft, topRight, bottomRight, vec4(debugColor, 1.0f));
+
+		// Draw anchors:
+		SetBlendFunc(BlendFunc::Linear);
+		//Primitives.DrawLine(, , vec4(debugColor, 0.5f));
+		//Primitives.DrawLine(, , vec4(debugColor, 0.5f));
+		//Primitives.DrawLine(, , vec4(debugColor, 0.5f));
+		//Primitives.DrawLine( , vec4(debugColor, 0.5f));
+	}
+#endif
+
+	void Widget::UpdateBounds(const Widget& parent)
+	{
+		const float offsetFromTop    = parent.absoluteBounds.height * top.anchor + top.offset;
+		const float offsetFromBottom = parent.absoluteBounds.height * bottom.anchor + bottom.offset;
+		const float offsetFromLeft   = parent.absoluteBounds.width * left.anchor + left.offset;
+		const float offsetFromRight  = parent.absoluteBounds.width * right.anchor + right.offset;
+
+		const float topEdge    = (parent.absoluteBounds.y + parent.absoluteBounds.height) - offsetFromTop;
+		const float bottomEdge = parent.absoluteBounds.y + offsetFromBottom;
+		const float leftEdge   = parent.absoluteBounds.x + offsetFromLeft;
+		const float rightEdge  = (parent.absoluteBounds.x + parent.absoluteBounds.width) - offsetFromRight;
+
+		absoluteBounds.x = leftEdge;
+		absoluteBounds.y = bottomEdge;
+		absoluteBounds.width = rightEdge - leftEdge;
+		absoluteBounds.height = topEdge - bottomEdge;
+
+		// The actual position of the entity is the pivot (center of the widget).
+		owner.position.x = absoluteBounds.x + absoluteBounds.width * 0.5f;
+		owner.position.y = absoluteBounds.y + absoluteBounds.height * 0.5f;
+
+		UpdateContent();
+
+		for (Entity::Ptr& child : owner.Get<Hierarchy>().GetChildren())
+		{
+			if (auto* childWidget = child->Try<Widget>())
+			{
+				childWidget->UpdateBounds(*this);
+			}
+		}
+	}
+}

gemcutter/GUI/Widget.h
@@ -1,0 +1,57 @@
+// Copyright (c) 2021 Emilian Cioca
+#pragma once
+#include "gemcutter/Entity/Entity.h"
+#include "gemcutter/GUI/Rectangle.h"
+
+namespace gem
+{
+	struct Edge
+	{
+		float anchor = 0.0f;
+		float offset = 0.0f;
+	};
+
+	// A generic UI element sized with respect to its parent widget.
+	class Widget : public Component<Widget>
+	{
+	public:
+		Widget(Entity& owner);
+
+		// Helper function which sets anchors to maintain the target size and position.
+		// Position is in the space of the parent widget.
+		void SetTransform(vec2 position, vec2 size);
+
+		// Sets the anchors to the full space available in the parent widget.
+		// This is the default behaviour.
+		void FitToParent();
+
+		Entity::Ptr CreateChildWidget();
+
+		const Rectangle& GetAbsoluteBounds() const { return absoluteBounds; }
+
+		static void UpdateAll();
+
+		// Called when the bounds of the widget are updated.
+		// This is where specific widgets can adjust their content.
+		virtual void UpdateContent() {}
+
+#ifdef _DEBUG
+		void DrawOutline() const;
+		vec3 debugColor;
+#endif
+
+		// Clips the rendering of child widgets within the bounds of this widget.
+		bool clipChildren = true;
+
+		Edge top;
+		Edge bottom;
+		Edge left;
+		Edge right;
+
+	private:
+		void UpdateBounds(const Widget& parent);
+
+		// World-space demensions of the widget
+		Rectangle absoluteBounds;
+	};
+}