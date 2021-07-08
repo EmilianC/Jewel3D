EdenEast/Gemcutter

Renamed Image resource to RawImage to avoid future conflicts

Tools/TextureEncoder/TextureEncoder.cpp
@@ -137,7 +137,7 @@ bool TextureEncoder::Convert(std::string_view source, std::string_view destinati
 	const Jwl::TextureWrap wrapX = Jwl::StringToTextureWrap(metadata.GetString("wrap_x"));
 	const Jwl::TextureWrap wrapY = Jwl::StringToTextureWrap(metadata.GetString("wrap_y"));
 
-	auto image = Jwl::Image::Load(source, !isCubemap, isSRGB);
+	auto image = Jwl::RawImage::Load(source, !isCubemap, isSRGB);
 	if (image.data == nullptr)
 		return false;
 

EmilianC/Jewel3D

tools/TextureEncoder/TextureEncoder.cpp
@@ -137,7 +137,7 @@ bool TextureEncoder::Convert(std::string_view source, std::string_view destinati
 	const gem::TextureWrap wrapX = gem::StringToTextureWrap(metadata.GetString("wrap_x"));
 	const gem::TextureWrap wrapY = gem::StringToTextureWrap(metadata.GetString("wrap_y"));
 
-	auto image = gem::Image::Load(source, !isCubemap, isSRGB);
+	auto image = gem::RawImage::Load(source, !isCubemap, isSRGB);
 	if (image.data == nullptr)
 		return false;
 
@@ -199,7 +199,7 @@ bool TextureEncoder::Convert(std::string_view source, std::string_view destinati
 				}
 			}
 		};
-		
+
 		copyFace(data, faceSize * 2, faceSize);						// +X
 		copyFace(data + 1 * textureSize, 0, faceSize);				// -X
 		copyFace(data + 2 * textureSize, faceSize, 0);				// +Y